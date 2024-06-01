2742f7ea8e243184ab09aa779b4b283b462fa1fc
Jeff Kuo <jrkuo2015@berkeley.edu>
2024-05-31 17:04:17 -0700

    downgrade to python3.11

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
-python3.12 -m pip install virtualenv
-python3.12 -m virtualenv venv3.12
-python3.12 -m pip install poetry
-python3.12 -m poetry add beautifulsoup4 pandas loguru tqdm requests google-api-python-client oauth2client google-auth gspread
+python -m pip install virtualenv
+python -m virtualenv venv3.11
+python -m pip install poetry
+python -m poetry add beautifulsoup4 pandas loguru tqdm requests google-api-python-client oauth2client google-auth gspread
