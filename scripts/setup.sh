bcb1f5c4375b0e699f3431cac43e8f915bd16d4b
Jeff Kuo <jrkuo2015@berkeley.edu>
2024-06-06 16:17:09 -0700

    update build

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,4 @@ python -m pip install virtualenv
 python -m virtualenv venv3.11
 python -m pip install poetry
 python -m poetry add beautifulsoup4 pandas loguru tqdm requests google-api-python-client oauth2client google-auth gspread
+python -m pip install -e .
